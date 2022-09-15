b8ba8acc6e0c792ccde25a9b5df3bed3df0bd11e
bakytow3 <bakytovnurislam38@gmail.com>
2022-09-15 14:49:01 +0600

    Initial commit

---
+++ src/Box.java
@@ -0,0 +1,12 @@
+public class Box <T> {
+
+
+
+    public  static <T> void Box(T type) {
+
+        System.out.println(type);
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+Box<String>stringBox=new Box<>();
+stringBox.Box("RRRR");
+        
+    }
+}
